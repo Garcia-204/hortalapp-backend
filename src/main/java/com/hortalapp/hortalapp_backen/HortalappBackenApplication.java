d49512871c2721ea4e6598ab7a5a4e1f9742fb64
Anderson Garcia <andersonlizcano2004@gmail.com>
2026-07-04 13:54:23 -0500

    inicio del proyecto Hortalapp

---
+++ src/main/java/com/hortalapp/hortalapp_backen/HortalappBackenApplication.java
@@ -0,0 +1,13 @@
+package com.hortalapp.hortalapp_backen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HortalappBackenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HortalappBackenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hortalapp/hortalapp_backen/HortalappBackenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hortalapp.hortalapp_backen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HortalappBackenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
